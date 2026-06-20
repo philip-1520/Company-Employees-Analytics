d6d7cde47cc5a2a35dac49676eef9b09c3643956
Philip Ribeiro Lima <ribeirolima1520@gmail.com>
2026-06-20 20:38:01 -0300

    Update performance rating.sql

--- performance rating.sql
+++ performance rating.sql
@@ -4,7 +4,10 @@ Business insight: this is a valuable information to plan perfomance-rewarding pr
 */
 
 WITH employees_rank AS (
-	SELECT *, TIMESTAMPDIFF(DAY, start_date, CURRENT_DATE()) AS tenure_days, RANK() OVER (PARTITION BY country, center ORDER BY job_rate DESC, start_date ASC) AS employee_rank
+	SELECT 	*,
+			TIMESTAMPDIFF(DAY, start_date, CURRENT_DATE()) AS tenure_days,
+			RANK() OVER (PARTITION BY country, center
+						ORDER BY job_rate DESC, start_date ASC) AS employee_rank
     FROM employees)
 
 SELECT country,
