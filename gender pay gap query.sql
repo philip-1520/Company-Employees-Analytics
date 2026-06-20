83a9f45ab6d80c011be75fb8057b0715f294074e
Philip Ribeiro Lima <ribeirolima1520@gmail.com>
2026-06-20 20:19:15 -0300

    Create gender pay gap query.sql

---
+++ gender pay gap query.sql
@@ -0,0 +1,23 @@
+/*
+Goal: to answer "What is gender pay gap in each of the countries the company operates?"
+Business insight: it helps the company companies to target gender-equality campaigns.
+*/
+
+WITH
+female_avg_salary AS (
+	SELECT country, AVG(annual_salary) AS female_avg_salary
+	FROM employees
+	WHERE gender = 'Female'
+    GROUP BY country),
+male_avg_salary AS (
+	SELECT country, AVG(annual_salary) AS male_avg_salary
+	FROM employees
+	WHERE gender = 'Male'
+    GROUP BY country)
+
+SELECT female.country, male.male_avg_salary, female.female_avg_salary, (male.male_avg_salary - female.female_avg_salary) AS pay_gap
+FROM male_avg_salary male
+JOIN female_avg_salary female
+ON female.country = male.country
+GROUP BY female.country
+ORDER BY pay_gap DESC;
